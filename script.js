7877ae9d0377a36e6a2ebdf9c818208998f40d94
ajensen3point14 <113062389+ajensen3point14@users.noreply.github.com>
2023-03-10 17:28:43 -0700

    function to add task to task list

--- script.js
+++ script.js
@@ -26,3 +26,25 @@ form.addEventListener('submit', function(e) {
 	// reset form
 	form.reset();
 });
+
+// function to add task to task list
+function addTask(task) {
+	// create new list item element
+	const li = document.createElement('li');
+
+	// set inner HTML of list item element
+	li.innerHTML = `
+		<h3>${task.name}</h3>
+		${task.details ? `<p>${task.details}</p>` : ''}
+		<p>Due Date: ${task.dueDate}</p>
+		<button class="complete-button">Complete Task</button>
+		<button class="delete-button">Delete Task</button>
+	`;
+
+	// add event listener to complete button
+	const completeButton = li.querySelector('.complete-button');
+	completeButton.addEventListener('click', function() {
+		// move task from task list to completed tasks
+		li.classList.add('completed');
+		completedTasks.appendChild(li);
+	});
